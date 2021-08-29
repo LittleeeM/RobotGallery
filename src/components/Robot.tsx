104e96c376ffdc02f1ff204725c2ef628d1d1d4e
littleeeM <yizi.amanda@gmail.com>
2021-08-29 15:50:50 +0800

    First commit

---
+++ src/components/Robot.tsx
@@ -0,0 +1,18 @@
+import React, { Props } from 'react';
+
+interface RobotProps {
+  id: number;
+  name: string;
+  email: string
+}
+
+const Robot : React.FC<RobotProps> = ({id, name, email}) => {
+
+  return <li>
+    <img alt="robot" src={`https://robohash.org/${id}`} />
+    <h2>{name}</h2>
+    <p>{email}</p>
+  </li>;
+};
+
+export default Robot;  
